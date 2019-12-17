1bc1ddde2ad2814a92c1969ac2d872e1de93c89a
odddyyy <56987696+odddyyy@users.noreply.github.com>
2019-12-17 07:51:24 +0700

    adding exercise 4

---
+++ Exercise4/exercise4.js
@@ -0,0 +1,44 @@
+var hari = 14;
+var bulan = 8;
+var tahun = 1955;
+
+switch(bulan){
+    case 1:
+        bulan = ` January `;
+        break;
+    case 2:
+        bulan = ` February `;
+        break;
+    case 3:
+        bulan = ` March `;
+        break;
+    case 4:
+        bulan = ` April `;
+        break;
+    case 5:
+        bulan = ` May `;
+        break;
+    case 6:
+        bulan = ` June `;
+        break;
+    case 7:
+        bulan = ` July `;
+        break;
+    case 8:
+        bulan = ` August `;
+        break;
+    case 9:
+        bulan = ` September `;
+        break;
+    case 10:
+        bulan = ` October `;
+        break;
+    case 11:
+        bulan = ` November `;
+        break;
+    case 12:
+        bulan = ` December `;  
+        break;   
+}
+
+console.log(hari + bulan + tahun);
